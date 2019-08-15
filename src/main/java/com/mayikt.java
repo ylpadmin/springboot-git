1c3aac9ee4a96c778e9528fe767d9a68a116b68f
sublun <sublun@126.com>
2019-08-15 12:45:46 +0800

    我的第一个git

---
+++ src/main/java/com/mayikt.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @Author: 杨李平
+ * @Date: 2019/8/15 11:33
+ */
+public class mayikt {
+    public static void main(String[] args) {
+        System.out.println("git测试");
+    }
+}
